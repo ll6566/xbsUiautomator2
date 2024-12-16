5fcdbdcfa9c667651e278168bc5e27e9facd56f6
张炼武_o <o_zhanglianwu@cvte.com>
2024-12-16 17:42:11 +0800

    删除内容

--- utils.py
+++
@@ -1,9 +0,0 @@
-import os
-from time import sleep
-
-
-def wait_dev(dev, timeout_=60):
-    for t in range(timeout_):
-        if dev in os.popen("adb devices").read():
-            return True
-        sleep(1)
